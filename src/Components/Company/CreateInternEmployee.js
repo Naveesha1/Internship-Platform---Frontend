f8e6c6479500c9abdf0789d8be71c6dff313b397
Manoj99 <110526623+Naveesha1@users.noreply.github.com>
2025-05-26 12:46:17 +0530

    remove console log

--- src/Components/Company/CreateInternEmployee.js
+++ src/Components/Company/CreateInternEmployee.js
@@ -1,69 +1,3 @@
-// import React from "react";
-
-// const CreateInternEmployee = ({
-//   newMentor,
-//   handleChange,
-//   handleAddMentor,
-//   closeModal,
-// }) => {
-//   return (
-//     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-//       <div className="bg-white p-6 rounded-lg shadow-lg w-96">
-//         <h2 className="text-lg font-bold mb-4">Add Students</h2>
-//         <input
-//           type="text"
-//           name="name"
-//           placeholder="Name"
-//           value={newMentor.name}
-//           onChange={handleChange}
-//           className="w-full mb-2 p-2 border rounded"
-//         />
-//         <input
-//           type="text"
-//           name="position"
-//           placeholder="Position"
-//           value={newMentor.position}
-//           onChange={handleChange}
-//           className="w-full mb-2 p-2 border rounded"
-//         />
-//         <input
-//           type="email"
-//           name="email"
-//           placeholder="Email"
-//           value={newMentor.email}
-//           onChange={handleChange}
-//           className="w-full mb-2 p-2 border rounded"
-//         />
-//         <input
-//           type="password"
-//           name="password"
-//           placeholder="Password"
-//           value={newMentor.password}
-//           onChange={handleChange}
-//           className="w-full mb-2 p-2 border rounded"
-//         />
-//         <div className="flex justify-end space-x-2 mt-4">
-//           <button
-//             onClick={closeModal}
-//             className="bg-gray-400 text-white px-4 py-2 rounded-lg hover:bg-gray-500 transition-colors"
-//           >
-//             Cancel
-//           </button>
-//           <button
-//             onClick={handleAddMentor}
-//             className="bg-[#45A29E] text-white px-4 py-2 rounded-lg hover:bg-[#367c79] transition-colors"
-//           >
-//             Assign 
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CreateInternEmployee;
-
-
 import axios from "axios";
 import React, { useEffect, useState, useContext } from "react";
 import {
@@ -196,7 +130,6 @@ const CreateInternEmployee = ({
       setIsSubmitting(false);
     }
   };
-  console.log(newStudent);
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-96 max-w-full mx-4">
